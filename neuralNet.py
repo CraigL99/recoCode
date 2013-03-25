2e020d1162e3e7f41809121083dc2fb876022264
Craig Lehmann <clehmann@uoguelph.ca>
2013-03-25 14:17:19 -0400

    so far so good, about to update error function

--- neuralNet.py
+++ neuralNet.py
@@ -49,56 +49,47 @@ def readData( filename, n_users, n_items ):
     return shared_sparseData, shared_rowInd, shared_colInd, actualMatrix
 
 def main():
-	#global config variables
-	learning_rate = 0.0001
-	n_users = 943
-	n_items = 1682
-	n_fac = 30
-	batch_size = 10000 #must be a multiple of the total number of ratings
+    #global config variables
+    learning_rate = 0.0001
+    n_users = 943
+    n_items = 1682
+    n_fac = 30
+    batch_size = 10000 #must be a multiple of the total number of ratings
 
-	#read in the data and convert it to bit vectors:
-	data = readData(filename='u.data', n_users=n_users, n_items=n_items)
-	s_d = data[0]
-	r_d = data[1]
-	c_d = data[2]
+    #read in the data and convert it to bit vectors:
+    data = readData(filename='u.data', n_users=n_users, n_items=n_items)
+    s_d = data[0]
+    r_d = data[1]
+    c_d = data[2]
 
-	#create a matrix to hold user bit vectors, size 0..99 999 x 0..1681
-	userBitMatrix = np.zeros((s_d.get_value().shape[0], n_users), dtype=int)
-	userBitMatrix[np.arange(s_d.get_value().shape[0]), (r_d.get_value()).astype(int)] = 1
+    #create a matrix to hold user bit vectors, size 0..99 999 x 0..1681
+    userBitMatrix = np.zeros((s_d.get_value().shape[0], n_users), dtype=theano.config.floatX)
+    userBitMatrix[np.arange(s_d.get_value().shape[0]), (r_d.get_value()).astype(int)] = 1
 
-	#create a matrix to hold user bit vectors, size 0..99 999 x 0..942
-	itemBitMatrix = np.zeros((s_d.get_value().shape[0], n_items), dtype=int)
-	itemBitMatrix[np.arange(s_d.get_value().shape[0]), (r_d.get_value()).astype(int)] = 1
-	
-	#cast rows to integers so they can be used as usual for indexing. (this used to be done within
-		#the read method however in order to create the bitMatrix, had to do it afterwards)
+    #create a matrix to hold user bit vectors, size 0..99 999 x 0..942
+    itemBitMatrix = np.zeros((s_d.get_value().shape[0], n_items), dtype=theano.config.floatX)
+    itemBitMatrix[np.arange(s_d.get_value().shape[0]), (r_d.get_value()).astype(int)] = 1
 
-	r_d = T.cast(r_d, 'int32') 
-	c_d = T.cast(c_d, 'int32')
+    #cast rows to integers so they can be used as usual for indexing. (this used to be done within
+    #the read method however in order to create the bitMatrix, had to do it afterwards)
+    #TODO is this needed?
 
-	#compute number of baches
-	n_train_batches = s_d.get_value().shape[0] / batch_size
+    #compute number of baches
+    n_train_batches = s_d.get_value().shape[0] / batch_size
 
+    print "Number of ratings: " + str(s_d.get_value().shape[0])
+    print "Batch size: " + str(batch_size)
+    print "n_train_batchs: " + str(n_train_batches)
 
-	print "Number of ratings: " + str(s_d.get_value().shape[0])
-	print "Batch size: " + str(batch_size)
-	print "n_train_batchs: " + str(n_train_batches)
+    model = MatrixFactorization(n_users=n_users, n_items=n_items, n_fac=n_fac)
+    model.U.set_value(0.01 * np.random.randn(n_users, n_fac))
+    model.V.set_value(0.01 * np.random.randn(n_fac, n_items))
 
-	"""for each batch
-			-convert the dataset to rowVectors, symbolicly
-			-dot that with U/V, symbolicly
-			-take the two new matricis and multiply them together elementwise,
-			-sum the rows to create a enw vector, sybolicaly
-			-compute the cost as the squared error as the actual vector - the created, squared
-			-take the gradient of cost wrt u and v and then subtract that amount from the current u and v	
-	"""
-	#TODO need GT history here.
+    #update cost, the squared error function and go from there. 
+    cost = 7
 
-	"""model = MatrixFactorization(n_users=n_users, n_items=n_items, n_fac=n_fac)
-
-
-    cost = model.squared_error(sparseData, rowInd, colInd) + L2 * lambda_L2 + L1 * lambda_L1
-	g_U = T.grad(cost=cost, wrt=model.U)
+    """
+    g_U = T.grad(cost=cost, wrt=model.U)
     g_V = T.grad(cost=cost, wrt=model.V)
 
     updates = {model.U: model.U - learning_rate * g_U, \
@@ -110,11 +101,42 @@ def main():
                                     sparseData: s_d[index * batch_size: (index + 1) * batch_size],
                                     rowInd: r_d[index * batch_size: (index + 1) * batch_size],
                                     colInd: c_d[index * batch_size: (index + 1) * batch_size] })
+    """ 
+    index = T.lscalar()
+    for i in xrange(50):
+        print i
+
+    """for each batch
+    	-convert the dataset to rowVectors, symbolicly
+    	-dot that with U/V, symbolicly
+    	-take the two new matricis and multiply them together elementwise,
+    	-sum the rows to create a enw vector, sybolicaly
+    	-compute the cost as the squared error as the actual vector - the created, squared
+    	-take the gradient of cost wrt u and v and then subtract that amount from the current u and v	
+    """
+    #TODO need GT history here.
+
+    """model = MatrixFactorization(n_users=n_users, n_items=n_items, n_fac=n_fac)
+
+
+    cost = model.squared_error(sparseData, rowInd, colInd) + L2 * lambda_L2 + L1 * lambda_L1
+    g_U = T.grad(cost=cost, wrt=model.U)
+    g_V = T.grad(cost=cost, wrt=model.V)
+
+    updates = {model.U: model.U - learning_rate * g_U, \
+           model.V: model.V - learning_rate * g_V}
+
+    train_model = theano.function(inputs=[index],
+                              outputs=cost,
+                              updates=updates, givens={
+                                sparseData: s_d[index * batch_size: (index + 1) * batch_size],
+                                rowInd: r_d[index * batch_size: (index + 1) * batch_size],
+                                colInd: c_d[index * batch_size: (index + 1) * batch_size] })
 
     for i in range(50):
-    	for minibatch_index in xrange(n_train_batches):
-            avg_cost = train_model(minibatch_index)
-            print str(i) + ":"+str(minibatch_index) + ' error: %4.4f' % avg_cost
+    for minibatch_index in xrange(n_train_batches):
+        avg_cost = train_model(minibatch_index)
+        print str(i) + ":"+str(minibatch_index) + ' error: %4.4f' % avg_cost
     """
 
 if __name__ == '__main__':
